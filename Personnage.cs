759701b13118718ceafa185d89cdc9e5e6483450
PatriceHANNE <patrice.hanne@gmail.com>
2024-01-07 22:34:41 +0100

    Ajoutez des fichiers projet.

---
+++ Personnage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniJeu
+{
+    internal class Personnage
+    {
+        // Attributs
+        private int vie;
+        private int xp;
+        private string nom;
+
+        // Getters / Setters
+        public int Vie { get => vie; set => vie = value; }
+        public int Xp { get => xp; set => xp = value; }
+        public string Nom { get => nom; set => nom = value; }
+
+
+
+        // Constructeur
+        public Personnage()
+        {
+            vie = 10;
+            xp = 0;
+            nom = "Perso";
+        }
+
+        public Personnage(int v, int x, string n)
+        {
+            Vie = v;
+            Xp = x;
+            Nom = n;
+        }
+
+        // Methodes
+        public void sePresenter()
+        {
+            Console.WriteLine("Bonjour, je suis " + nom);
+        }
+
+        // Autres Methodes
+        public int retournerVie()
+        {
+            return vie;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniJeu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mon First game of the Hello World");
+        }
+    }
+}
